734b5cf32687b75409c9078bde48779b4ec47f77
IgnasJJK <ignas.kaveckas@gmail.com>
2024-12-15 19:28:17 +0200

    Add camera smoothing

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,13 +3,17 @@ using UnityEngine.InputSystem;
 
 public class CameraControl : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject focalPoint;
 
     InputAction mouseLookAction;
 
     [SerializeField] float startingDistanceToTarget = 5f;
     [SerializeField] Vector2 lookSpeed = new Vector2(1.0f, 1.0f);
+    [SerializeField] float minVerticalLookAngle = -60f;
     [SerializeField] float maxVerticalLookAngle = 70f;
+    [SerializeField] float smoothingSpeed = 0.1f;
+
+    [SerializeField] LayerMask environmentLayerMask = (1 << 7);
 
     public bool invertVertical = true;
     public bool invertHorizontal = false;
@@ -17,6 +21,8 @@ public class CameraControl : MonoBehaviour
     float distanceToTarget;
     // x -- rotation around x (pitch); y -- rotation around y (yaw)
     Vector2 angle;
+
+    Vector3 targetPosition;
 
     void Start()
     {
@@ -31,17 +37,22 @@ public class CameraControl : MonoBehaviour
         // NOTE: Looking axes are correct. Moving the mouse horizontally (x), 
         // will rotate around the y axis. Analogous for vertical (y).
         angle.x += lookSpeed.x * mouseLook.y * (invertVertical ? -1 : 1);
-        angle.x = Mathf.Clamp(angle.x, -maxVerticalLookAngle, maxVerticalLookAngle);
+        angle.x = Mathf.Clamp(angle.x, minVerticalLookAngle, maxVerticalLookAngle);
         angle.y += lookSpeed.y * mouseLook.x * (invertHorizontal ? -1 : 1);
 
         Quaternion pitchYawRotation = Quaternion.Euler(angle.x, angle.y, 0);
 
         Vector3 cameraOffset = new Vector3(0, 0, -distanceToTarget);
 
-        Vector3 pos = target.transform.position;
-        pos += pitchYawRotation * cameraOffset;
+        targetPosition = focalPoint.transform.position;
+        targetPosition += pitchYawRotation * cameraOffset;
 
-        transform.position = pos;
-        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+        if(Physics.Raycast(focalPoint.transform.position, targetPosition - focalPoint.transform.position, out RaycastHit hitInfo, distanceToTarget, environmentLayerMask))
+        {
+            targetPosition = hitInfo.point;
+        }
+        
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothingSpeed);
+        transform.rotation = Quaternion.LookRotation(focalPoint.transform.position - transform.position);
     }
 }
